6b67ba7dca63cd32ffddb43272b738f58fb91e6f
Joshua Bohde <bohdetroit@gmail.com>
2018-10-06 08:05:37 -0400

    changed server.js file

--- server.js
+++ server.js
@@ -20,16 +20,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 }
 // Add API Routes
 app.use("/api", routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
