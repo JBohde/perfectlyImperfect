80a9beed281ef61f520025eb28353e4d733a1b8f
Joshua Bohde <bohdetroit@gmail.com>
2018-10-08 21:38:30 -0400

    fixed card overflow

--- client/src/components/BlogCard/BlogCard.js
+++ client/src/components/BlogCard/BlogCard.js
@@ -6,7 +6,7 @@ const BlogCard = props => (
   <div className="card">
     <img className="card-img-top" src={props.src} style={{height: 12+ 'rem'}} alt="Card cap" />
     <div className="card-body">
-      <h5 className="card-title">{props.blog_title}</h5>
+      <h6 className="card-title">{props.blog_title}</h6>
       <h6>{props.blog_date}</h6>
       {props.blog_text}
       <Link to={`/blog/${props.link}`} className="btn btn-primary" id="main-blog-read">READ MORE</Link>
